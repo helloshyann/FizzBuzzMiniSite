b73abd3279870201117c37525ae0b43a00ed4ff8
Shyann <helloshyann@gmail.com>
2020-10-07 22:41:47 -0700

    Initial commit

---
+++ Scripts/Custom.js
@@ -0,0 +1,36 @@
+﻿
+
+
+////FizzBuzz Code
+function fizzBuzz() {
+    let firstNum = document.getElementById('fizzNum').value;
+    let secondNum = document.getElementById('buzzNum').value;
+    let returnValue = [];
+    for (let i = 1; i <= 100; i++) {
+        if (i % firstNum == 0 && i % secondNum == 0) {
+            returnValue.push("<span class='fizzbuzz ffont'> Fizz</span><span class='fizzbuzz bfont'> Buzz</span>");
+        }
+        else if (i % firstNum == 0) {
+            returnValue.push("<span class='fbstyle'> Fizz</span>");
+        }
+        else if (i % secondNum == 0) {
+            returnValue.push("<span class='istyle'> Buzz</span>");
+        }
+        else {
+            returnValue.push(" " + i);
+        }
+    }
+    return returnValue;
+}
+
+
+
+
+//Call FizzBuzz
+function fizzle() {
+    let result = "";
+    let value1 = document.getElementById('fizzNum').value;
+    let value2 = document.getElementById('buzzNum').value;
+    result = fizzBuzz(value1, value2);
+    document.getElementById('result').innerHTML = `${result}`;
+}
