3f8bebdd1ba8b14bdd1a71b1299388df54980068
Shyann <helloshyann@gmail.com>
2020-10-11 04:25:01 -0700

    Added Grid

--- Scripts/Custom.js
+++ Scripts/Custom.js
@@ -24,21 +24,32 @@ function fizzBuzz() {
 
 
 
-//Call FizzBuzz
 function fizzle() {
-    let result = "";
-    let value1 = document.getElementById('fizzNum').value;
-    let value2 = document.getElementById('buzzNum').value;
+    //Grab the template from the HTML document
+    let output = [];
+    let headTemplate = document.getElementById('template-header');
+    let rowTemplate = document.getElementById('template-row-items');
 
-    if (value1 == "" || value2 == "") {
-        document.getElementById('result').innerHTML = 'You must enter two numbers to play Fizz Buzz.';
+    var templateHTML = rowTemplate.innerHTML;
+    var resultsHTML = headTemplate.innerHTML;
+    //Get the values I want to use
+    let val1 = document.getElementById('fizzNum').value;
+    let val2 = document.getElementById('buzzNum').value;
+    //Call the Fizz Buzz Function
+    output = fizzBuzz(val1, val2);
+    //Empty value restriction else function output
+    if (val1 == "" || val2 == "") {
+        document.getElementById('results').innerHTML = 'You must enter two numbers to play Fizz Buzz';        
     }
     else {
-
-        result = fizzBuzz(value1, value2);
-        document.getElementById('result').innerHTML = `${result}`;
+        for (i = 0; i < output.length; i += 5) {
+            resultsHTML += templateHTML.replace('{{val1}}', output[i])
+                .replace('{{val2}}', output[i + 1])
+                .replace('{{val3}}', output[i + 2])
+                .replace('{{val4}}', output[i + 3])
+                .replace('{{val5}}', output[i + 4]);
+        }
+        document.getElementById('results').innerHTML = resultsHTML;
+       
     }
 }
-
-
-
