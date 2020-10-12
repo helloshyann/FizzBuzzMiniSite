dc1040fd5690874b5ee73519936066c9fbded77c
Shyann <helloshyann@gmail.com>
2020-10-11 22:04:36 -0700

    code

--- Scripts/Custom.js
+++ Scripts/Custom.js
@@ -1,4 +1,4 @@
-﻿////FizzBuzz Code
+﻿//FizzBuzz Code
 function fizzBuzz() {
     let firstNum = document.getElementById('fizzNum').value;
     let secondNum = document.getElementById('buzzNum').value;
@@ -20,24 +20,21 @@ function fizzBuzz() {
     }
     return finalResult;
 }
-
-
-
-
+//FizzBuzz Button Function
 function fizzle() {
-    //Grab the template from the HTML document
+    
     let output = [];
     let headTemplate = document.getElementById('template-header');
     let rowTemplate = document.getElementById('template-row-items');
 
-    var templateHTML = rowTemplate.innerHTML;
-    var resultsHTML = headTemplate.innerHTML;
-    //Get the values I want to use
+    let templateHTML = rowTemplate.innerHTML;
+    let resultsHTML = headTemplate.innerHTML;
+    
     let val1 = document.getElementById('fizzNum').value;
     let val2 = document.getElementById('buzzNum').value;
-    //Call the Fizz Buzz Function
+
     output = fizzBuzz(val1, val2);
-    //Empty value restriction else function output
+
     if (val1 == "" || val2 == "") {
         document.getElementById('results').innerHTML = 'You must enter two numbers to play Fizz Buzz';        
     }
@@ -49,7 +46,6 @@ function fizzle() {
                 .replace('{{val4}}', output[i + 3])
                 .replace('{{val5}}', output[i + 4]);
         }
-        document.getElementById('results').innerHTML = resultsHTML;
-       
+        document.getElementById('results').innerHTML = resultsHTML;       
     }
 }
